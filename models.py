8ca087269590ac01e06810d1c0834ca626df3ce0
markallanjohnson <markallanjohnson1@gmail.com>
2021-02-17 09:47:38 -0700

    Models.py fixed per code review

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ quirks of the data set, such as missing names and unknown diameters.
 
 You'll edit this file in Task 1.
 """
-from helpers import cd_to_datetime, datetime_to_str
+import datetime
 
 
 class NearEarthObject:
@@ -48,12 +48,14 @@ class NearEarthObject:
         if self.name == '':
             self.name = None
 
-        if diameter == '':
+        self.diameter = diameter
+        if self.diameter == '':
             self.diameter = float('nan')
         else:
             self.diameter = float(diameter)
 
-        if hazardous == 'Y':
+        self.hazardous = hazardous
+        if self.hazardous == 'Y':
             self.hazardous = True
         else:
             self.hazardous = False
@@ -120,8 +122,24 @@ class CloseApproach:
             self.time = None
         else:
             self.time = cd_to_datetime(time)
-        self.distance = float(distance)
-        self.velocity = float(velocity)
+
+        # handles distance = ""
+        try:
+            self.distance = float(distance)
+        except:
+            self.distance = 0.0
+        else:
+            self.distance = float(distance)
+
+        # handles velocity = ""
+        try:
+            self.velocity = float(velocity)
+        except:
+            self.velocity = 0.0
+        else:
+            self.velocity = float(velocity)
+
+
         # Create an attribute for the referenced NEO, originally None.
         self.neo = neo
 
@@ -156,3 +174,35 @@ class CloseApproach:
         """Return `repr(self)`, a computer-readable string representation of this object."""
         return (f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, "
                 f"velocity={self.velocity:.2f}, neo={self.neo!r})")
+
+
+# from helpers.py
+def cd_to_datetime(calendar_date):
+    """Convert a NASA-formatted calendar date/time description into a datetime.
+
+    NASA's format, at least in the `cd` field of close approach data, uses the
+    English locale's month names. For example, December 31st, 2020 at noon is:
+
+        2020-Dec-31 12:00
+
+    This will become the Python object `datetime.datetime(2020, 12, 31, 12, 0)`.
+
+    :param calendar_date: A calendar date in YYYY-bb-DD hh:mm format.
+    :return: A naive `datetime` corresponding to the given calendar date and time.
+    """
+    return datetime.datetime.strptime(calendar_date, "%Y-%b-%d %H:%M")
+
+# from helpers.py
+def datetime_to_str(dt):
+    """Convert a naive Python datetime into a human-readable string.
+
+    The default string representation of a datetime includes seconds; however,
+    our data isn't that precise, so this function only formats the year, month,
+    date, hour, and minute values. Additionally, this function provides the date
+    in the usual ISO 8601 YYYY-MM-DD format to avoid ambiguities with
+    locale-specific month names.
+
+    :param dt: A naive Python datetime.
+    :return: That datetime, as a human-readable string without seconds.
+    """
+    return datetime.datetime.strftime(dt, "%Y-%m-%d %H:%M")
